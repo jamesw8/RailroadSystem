28baca7fc804889bb19d93177c62e216d6884d49
jamesw8 <jameswong8@gmail.com>
2017-12-20 22:41:39 -0500

    Added db conn

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template
-
+import pymysql
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+# create connection
+try:
+	with open('passwd', 'r') as pwd:
+		passwd = pwd.read().rstrip('\n')
+	conn = pymysql.connect(host='127.0.0.1', port=3306, user='F17336Pteam3', passwd=passwd, database='railroad1')
+	cur = conn.cursor()
+except:
+	print('ERROR: CANNOT RETRIEVE DB PASSWORD')
+	pass
+
+@app.route('/', methods=['GET']) 
 def index():
+	print(cur)
+	print('hi')
 	return render_template('index.html')
 
 if __name__ == '__main__':
