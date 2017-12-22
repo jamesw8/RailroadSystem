0c94133293acaab5fd44008ce84cc4f8c17e91ee
erickolbusz <erickolbusz@gmail.com>
2017-12-21 19:39:14 -0500

    fix typo

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def viewStations():
         results = cur.fetchall()
         print(results)
         return render_template('index.html', headers=headers, results=results)
-    return render_template('index.html' logged_in=is_logged_in())
+    return render_template('index.html', logged_in=is_logged_in())
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8003)
