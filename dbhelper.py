f9210c9617287d6ccb87736cb84af843b7b0858d
Armando <Armando024@live.com>
2017-12-22 02:17:06 -0500

    trying the insert of passangers info

--- dbhelper.py
+++ dbhelper.py
@@ -18,6 +18,12 @@ def auth_register(fname, lname, email, password, preferred_card_number, preferre
     if len(password) < 6:
         return (False, "Password must be at least 6 characters")
     #add to db
+    c = connect()
+    cur = c.cursor()
+    command = "INSERT INTO passangers (fname,lname,email,password,preferred_card_number,preferred_billing_address) VALUES  ("+fname+","+lname+ ","+email+ ","+password+ ","+preferred_Card_number+","+preferred_billing_address+") " 	
+    cur.execute('describe passangers;')
+    headers=cur.fetchall()
+    print(results)
     return (True, "Registration successful")    
     
 def auth_login(email, password):
