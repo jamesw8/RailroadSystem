dc2d511858b81db8d491d001f566f1c984d3ca9f
Armando <Armando024@live.com>
2017-12-22 21:22:11 -0500

    fix sql code v1

--- dbhelper.py
+++ dbhelper.py
@@ -7,8 +7,8 @@ def connect():
             conn = pymysql.connect(host='127.0.0.1', port=3306, user='F17336Pteam3', passwd=passwd, database='F17336Pteam3')
             return conn
     except IOError as e:
-            print('Error: %s', e)
-   	    return None
+	    print('Error: %s', e)
+	    return None
 
 def auth_register(fname, lname, email, password, preferred_card_number, preferred_billing_address):
     #check that email not already tied to another user
@@ -20,13 +20,8 @@ def auth_register(fname, lname, email, password, preferred_card_number, preferre
     #add to db
     c = connect()
     cur = c.cursor()
-    command = "INSERT INTO passengers (fname,lname,email,password,preferred_card_number,preferred_billing_address) VALUES  ("+fname+","+lname+ ","+email+ ","+password+ ","+preferred_Card_number+","+preferred_billing_address+") " 	
-    cur.execute('describe passengers;')
-    headers=cur.fetchall()
-    print(headers)
+    command = "INSERT INTO passengers (fname,lname,email,password,preferred_card_number,preferred_billing_address) VALUES  ("+fname+","+lname+ ","+email+ ","+password+ ","+preferred_Card_number+","+preferred_billing_address+");" 	
     cur.execute(command)
-    results=cur.fetchall()
-    print(results)
     return (True, "Registration successful")    
     
 def auth_login(email, password):
