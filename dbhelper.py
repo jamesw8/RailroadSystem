defd3db9f0ed3403af7a372909f498f3b52dff80
James Wong <jameswong8@gmail.com>
2017-12-22 22:10:58 -0500

    revert

--- dbhelper.py
+++ dbhelper.py
@@ -1,30 +1,30 @@
 import pymysql
 
 def connect():
-	try:
-		with open('passwd', 'r') as pwd:
-			passwd = pwd.read().rstrip('\n')
-			conn = pymysql.connect(host='127.0.0.1', port=3306, user='F17336Pteam3', passwd=passwd, database='F17336Pteam3')
-			return conn
-	except IOError as e:
-		print('Error: %s', e)
-	return None
+    try:
+        with open('passwd', 'r') as pwd:
+            passwd = pwd.read().rstrip('\n')
+            conn = pymysql.connect(host='127.0.0.1', port=3306, user='F17336Pteam3', passwd=passwd, database='F17336Pteam3')
+            return conn
+    except IOError as e:
+        print('Error: %s', e)
+    return None
 
 def auth_register(fname, lname, email, password, preferred_card_number, preferred_billing_address):
-	#check that email not already tied to another user
-	#return (False, "An account already exists with that email")
-	if '@' not in email:
-		return (False, "Invalid email")
-	if len(password) < 6:
-		return (False, "Password must be at least 6 characters")
-	#add to db
-	c = connect()
-	cur = c.cursor()
-	command = "INSERT INTO passengers (fname,lname,email,password,preferred_card_number,preferred_billing_address) VALUES  ("+fname+","+lname+ ","+email+ ","+password+ ","+preferred_Card_number+","+preferred_billing_address+");"    
-	cur.execute(command)
-	return (True, "Registration successful")    
-	
+    #check that email not already tied to another user
+    #return (False, "An account already exists with that email")
+    if '@' not in email:
+        return (False, "Invalid email")
+    if len(password) < 6:
+        return (False, "Password must be at least 6 characters")
+    #add to db
+    c = connect()
+    cur = c.cursor()
+    command = "INSERT INTO passengers (fname,lname,email,password,preferred_card_number,preferred_billing_address) VALUES  ("+fname+","+lname+ ","+email+ ","+password+ ","+preferred_Card_number+","+preferred_billing_address+");"    
+    cur.execute(command)
+    return (True, "Registration successful")    
+    
 def auth_login(email, password):
-	#check if combo in db
-	#return (False, "Email and/or password incorrect")
-	return (True, "Login successful", "Jeff")
+    #check if combo in db
+    #return (False, "Email and/or password incorrect")
+    return (True, "Login successful", "Jeff")
