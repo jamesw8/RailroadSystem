a123b3167f32502919ce30cbeab6e7399c1a74b7
Armando <Armando024@live.com>
2017-12-22 23:09:08 -0500

    delete some syntax

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def register():
         cur = c.cursor()
         command = "INSERT INTO passengers (fname,lname,email,password,preferred_card_number,preferred_billing_address) VALUES ('"+fname+"','"+lname+"','"+email+"','"+password+"','"+preferred_card_number+"','"+preferred_billing_address+"');"  
         cur.execute(command)
-        return render_template('index.html', headers=headers, results=results,logged_in=is_logged_in) 
+        return render_template('index.html',logged_in=is_logged_in) 
     return render_template('register.html',logged_in=is_logged_in() )
       #  filled_out = True
        # for field in [fname, lname, email, password, preferred_card_number, preferred_billing_address]:
