1c65644474dee86406a78b276f5fd7d6c5cd22da
Armando <Armando024@live.com>
2017-12-23 03:53:31 -0500

    reroute to correct link

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def makeReservation():
         print(request.path,url_for('makeReservation'))
         if not session.get('logged_in'):
                 flash("You need to log in or sign up to Book A Ticket")
-                return redirect(url_for('login'))
+                return redirect(url_for('f17336pteam3/login'))
         if request.method == 'POST':
                 command = request.form['command']
                 cur.execute('describe stations;')
