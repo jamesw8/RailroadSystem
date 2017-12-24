3b814e020989c499508c334454b6ecb178fed7ce
Armando <Armando024@live.com>
2017-12-24 17:24:52 -0500

    reg edits

--- dbhelper.py
+++ dbhelper.py
@@ -13,14 +13,17 @@ def connect():
 
 def auth_register(fname, lname, email, password, preferred_card_number, preferred_billing_address):
     #check that email not already tied to another user
-    #return (False, "An account already exists with that email")
+    c = connect()
+    cur = c.cursor()
+    check=cur.execute("select * FROM passengers WHERE email=%s;",(emai) )
+    if check is None:  
+        return (False, "An account already exists with that email")
+    
     if '@' not in email:
         return (False, "Invalid email")
     if len(password) < 6:
         return (False, "Password must be at least 6 characters")
     #add to db
-    c = connect()
-    cur = c.cursor()
     command="INSERT INTO passengers (fname,lname,email,password,preferred_card_number,preferred_billing_address) VALUES (%s,%s,%s,%s,%s,%s);"
     cur.execute(command,(fname,lname,email,generate_password_hash(password),preferred_card_number,preferred_billing_address))
     c.commit() 
