17289b947dd998343e4a1ae91fb8e0ea8beabe5e
James Wong <jameswong8@gmail.com>
2017-12-24 00:21:16 -0500

    Switch to tryt/except to accurately catch bug

--- app.py
+++ app.py
@@ -162,7 +162,11 @@ def checkTrip(train, start, end, travel_date):
         cost += queried_segment[3]
         # check if free seat
         cur.execute('SELECT * FROM seats_free WHERE train_id=' + str(train_id) + ' and segment_id=' + str(queried_segment[0]) + ' and seat_free_date="' + str(travel_date.year) + '-' + str(travel_date.month) + '-' + str(travel_date.day) + '";')
-        queried_seats = cur.fetchall()[0]
+        queried_seats = cur.fetchall()
+        try:
+            queried_seats = queried_seats[0]
+        except:
+            return False, 0
         free_seats = queried_seats[3]
         if free_seats <= 0:
             free_seat = False
