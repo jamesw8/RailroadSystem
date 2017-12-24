5f55e5f9cafefc4d0f675e37cee17ff1d552fc04
Armando <Armando024@live.com>
2017-12-24 05:27:05 -0500

    s

--- app.py
+++ app.py
@@ -130,8 +130,9 @@ def viewTrains():
         return redirect('/f17336pteam3'+url_for('index'))
     if request.method == 'POST':
         # handle reserve
-
-        return render_template('index.html', logged_in=is_logged_in(), headers=headers, results=results)
+        flash( request.form['dep_station']) 
+        return render_template('trains.html', logged_in=is_logged_in())
+        #return render_template('index.html', logged_in=is_logged_in(), headers=headers, results=results)
     return render_template('trains.html', logged_in=is_logged_in())
 
 @app.route('/mytrips', methods=['GET'])
