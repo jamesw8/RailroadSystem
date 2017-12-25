63726e185524f315fcf5273db81e54ab9a0fda7f
Armando <Armando024@live.com>
2017-12-24 19:33:58 -0500

    timestampt

--- app.py
+++ app.py
@@ -144,7 +144,8 @@ def viewTrains():
         results=cur.fetchone()
         #inserting into reservations 
         command="INSERT INTO reservations (reservation_date,paying_passenger_id,card_number,billing_address) VALUES (%s,%s,%s,%s);"
-        stampdate=str(session.get('date').year) + '-' + str(session.get('date').month) + '-' + str(session.get('date').day)+" "+allinfo[2]         
+        #stampdate=str(session.get('date').year) + '-' + str(session.get('date').month) + '-' + str(session.get('date').day)+" "+allinfo[2]         
+        stampdate="CURRENT_TIMESTAMP" 
         cur.execute(command,(stampdate,passenger_id,results[0],results[1]))
         c.commit()
         #getting reservation_id
