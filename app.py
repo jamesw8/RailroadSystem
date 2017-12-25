23b0678a12033da52fc954206153a994c63dd74c
Armando <Armando024@live.com>
2017-12-24 20:42:20 -0500

    ident1

--- app.py
+++ app.py
@@ -135,17 +135,17 @@ def viewTrains():
             flash('You need to log in or register to book this ticket')
             return redirect('/f17336pteam3'+url_for('viewTrains'))
         # handle confirmatio page
-    c = db.connect()
-    cur = c.cursor()
-    cur.execute('SELECT * FROM stations_copy;')
-    stations = cur.fetchall()
-    cur.execute('SELECT * FROM reservations WHERE paying_passenger_id=' + str(session['id']) + ' ORDER BY reservation_date DESC LIMIT 1;')
-    reservations = cur.fetchall()
-    trips = []
-    for reservation in reservations:
-        cur.execute('SELECT * FROM trips WHERE reservation_id=' + str(reservation[0]) + ';')
-        trip = cur.fetchone()
-        trips.append({
+        c = db.connect()
+        cur = c.cursor()
+        cur.execute('SELECT * FROM stations_copy;')
+        stations = cur.fetchall()
+        cur.execute('SELECT * FROM reservations WHERE paying_passenger_id=' + str(session['id']) + ' ORDER BY reservation_date DESC LIMIT 1;')
+        reservations = cur.fetchall()
+        trips = []
+        for reservation in reservations:
+            cur.execute('SELECT * FROM trips WHERE reservation_id=' + str(reservation[0]) + ';')
+            trip = cur.fetchone()
+            trips.append({
             'reservation_id': reservation[0],
             'reservation_date': reservation[1],
             'departure_station': stations[int(trip[2])][1],
@@ -153,7 +153,8 @@ def viewTrains():
             'trip_date': trip[1],
             'fare': trip[5]
             })
-    return render_template('confirmation.html', trips=trips, logged_in=is_logged_in())
+        return render_template('confirmation.html', trips=trips, logged_in=is_logged_in())
+    return render_template('trains.html', logged_in=is_logged_in())
         #handle reserve code
     '''    c = db.connect()
         cur = c.cursor()
@@ -207,7 +208,6 @@ def viewTrains():
         reduceSeat(train, segments, tHd)
         c.commit() 
         return redirect('/f17336pteam3'+url_for('viewTrips')) ''' 
-   return render_template('trains.html', logged_in=is_logged_in())
 
 @app.route('/mytrips', methods=['GET'])
 def viewTrips():
